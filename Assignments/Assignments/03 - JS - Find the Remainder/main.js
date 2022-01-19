b4fa4944d0da54d0da0a385f0f66f8e2d7667150
Caitlynn Journey <64987324+chevon12@users.noreply.github.com>
2022-01-19 10:12:52 -0500

    Initial Commit

---
+++ Assignments/Assignments/03 - JS - Find the Remainder/main.js
@@ -0,0 +1,3 @@
+//Create a function that will take any number, divide the number by any number,
+//and the output should be how many times the number
+//got divided as well as the modulus of the numbers you are dividing.
