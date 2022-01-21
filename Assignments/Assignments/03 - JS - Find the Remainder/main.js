461469ef710be4fa72898eae7db3ddcb7adadeaf
Caitlynn Journey <64987324+chevon12@users.noreply.github.com>
2022-01-20 20:51:02 -0500

    update

--- Assignments/Assignments/03 - JS - Find the Remainder/main.js
+++ Assignments/Assignments/03 - JS - Find the Remainder/main.js
@@ -1,3 +1,8 @@
 //Create a function that will take any number, divide the number by any number,
 //and the output should be how many times the number
 //got divided as well as the modulus of the numbers you are dividing.
+funciton divideNumber(dividend, divisior){
+
+}
+
+
