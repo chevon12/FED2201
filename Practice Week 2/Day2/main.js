84150c6807743918c80e4b11df17f7d27db22815
Caitlynn Journey <64987324+chevon12@users.noreply.github.com>
2022-01-26 16:30:00 -0500

    Assignment 06 Initial files

---
+++ Practice Week 2/Day2/main.js
@@ -0,0 +1,37 @@
+console.log("Hello World")
+// Variable Declaration
+
+var  x=; // you can reassign let and var
+let y =;
+const z = {
+  prop1: 1,
+  prop2: "A string"
+};
+
+function testFUnc (a,b){
+  let 
+  return 'first param (a):${a}\nsecond param(b):{b}\n a "var" variable (x): ${x}\n a let variable in the global scope (y): ${y} \na let variable in the local scope (z): ${z}'
+};
+// you can't reassign const
+console.log(" a let variable in the local scope of testFunc (z): ${z}");
+
+var functionname = () = > {
+  console.log("A defined arrow function with the one that was run
+  ");
+
+}
+
+var  anArrayOfColors = [
+  "red",
+  "yellow",
+  "green",
+  "blue",
+];
+  
+function putWordsInASrnyrnce(arr) = {
+  let newArr = arr.map(item => "the sky is $(item)")
+
+  return newArr;
+}
+
+console.log(putWordsINASentence(anArrayOfColors))
