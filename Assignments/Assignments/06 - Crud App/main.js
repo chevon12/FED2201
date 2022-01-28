e05e0c3fbbbd2d535e18959113b945b41a29f8b9
Caitlynn Journey <64987324+chevon12@users.noreply.github.com>
2022-01-28 10:34:09 -0500

    Update main.js

--- Assignments/Assignments/06 - Crud App/main.js
+++ Assignments/Assignments/06 - Crud App/main.js
@@ -1,29 +1,33 @@
-var app = new function() {
-  this.el = document.getElementById('tasks');
+var app = new (function () {
+  this.el = document.getElementById("tasks");
 
   this.tasks = [];
 
-  
-  
-  this.FetchAll = function() {
-    var data = '';
+  this.FetchAll = function () {
+    var data = "";
 
     if (this.tasks.length > 0) {
       for (i = 0; i < this.tasks.length; i++) {
-        data += '<tr>';
-        data += '<td>'+(i+1)+". " + this.tasks[i] + '</td>';
-        data += '<td><button onclick="app.Edit(' + i + ')"  class="btn btn-warning">Edit</button></td>';
-        data += '<td><button onclick="app.Delete(' + i + ')"  class="btn btn-danger">Delete</button></td>';
-        data += '</tr>';
+        data += "<tr>";
+        data += "<td>" + (i + 1) + ". " + this.tasks[i] + "</td>";
+        data +=
+          '<td><button onclick="app.Edit(' +
+          i +
+          ')"  class="btn btn-warning">Edit</button></td>';
+        data +=
+          '<td><button onclick="app.Delete(' +
+          i +
+          ')"  class="btn btn-danger">Delete</button></td>';
+        data += "</tr>";
       }
     }
 
     this.Count(this.tasks.length);
-    return this.el.innerHTML = data;
+    return (this.el.innerHTML = data);
   };
 
   this.Add = function () {
-    el = document.getElementById('add-todo');
+    el = document.getElementById("add-todo");
     // Get the value
     var task = el.value;
 
@@ -31,21 +35,21 @@ var app = new function() {
       // Add the new value
       this.tasks.push(task.trim());
       // Reset input value
-      el.value = '';
+      el.value = "";
       // Dislay the new list
       this.FetchAll();
     }
   };
 
   this.Edit = function (item) {
-    var el = document.getElementById('edit-todo');
+    var el = document.getElementById("edit-todo");
     // Display value in the field
     el.value = this.tasks[item];
     // Display fields
-    document.getElementById('edit-box').style.display = 'block';
+    document.getElementById("edit-box").style.display = "block";
     self = this;
 
-    document.getElementById('save-edit').onsubmit = function() {
+    document.getElementById("save-edit").onsubmit = function () {
       // Get value
       var task = el.value;
 
@@ -57,7 +61,7 @@ var app = new function() {
         // Hide fields
         CloseInput();
       }
-    }
+    };
   };
 
   this.Delete = function (item) {
@@ -67,33 +71,23 @@ var app = new function() {
     this.FetchAll();
   };
 
-  this.Count = function(data) {
-    var el   = document.getElementById('counter');
-    var name = 'Tasks';
+  this.Count = function (data) {
+    var el = document.getElementById("counter");
+    var name = "Tasks";
 
     if (data) {
-        if(data ==1){
-            name = 'Task'
-        }
-      el.innerHTML = data + ' ' + name ;
-    } 
-    else {
-      el.innerHTML = 'No ' + name;
+      if (data == 1) {
+        name = "Task";
+      }
+      el.innerHTML = data + " " + name;
+    } else {
+      el.innerHTML = "No " + name;
     }
   };
-  
-}
+})();
 
 app.FetchAll();
 
 function CloseInput() {
-  document.getElementById('edit-box').style.display = 'none';
+  document.getElementById("edit-box").style.display = "none";
 }
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-P
